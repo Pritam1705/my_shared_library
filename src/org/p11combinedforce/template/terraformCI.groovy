773811c095012ac11f63fa51f9fd6c2be115cc1a
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-11 10:16:02 +0530

    Update terraformCI.groovy

--- src/org/p11combinedforce/template/terraformCI.groovy
+++ src/org/p11combinedforce/template/terraformCI.groovy
@@ -6,10 +6,11 @@ def call(String url, String branch, String creds, String Dir = "") {
     
     def wsClean = new WsClean()
     def gitCheckOut = new GitCheckOut()
+    def terraformInit = new TerraformInit()
 
     // call method with correct arg
 
     wsClean().call()
     gitCheckOut(url, branch, creds).call()
+    terraformInit(Dir).call()
 }
-
