ebbc6783befbad3de80dc7f547f97bd51cea3080
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-11 09:36:45 +0530

    Create terraformCI.groovy

---
+++ src/org/p11combinedforce/template/terraformCI.groovy
@@ -0,0 +1,16 @@
+package org.p11combinedforce.terraformCI
+
+import org.p11combinedforce.terraformCI.*
+import org.p11combinedforce.common.*
+
+def call(String url, String branch, String creds, String Dir = "") {
+    
+    def wsClean = new WsClean()
+    def gitCheckOut = new GitCheckOut()
+
+    // call method with correct arg
+
+    wsClean().call()
+    gitCheckOut(url, branch, creds).call()
+}
+
