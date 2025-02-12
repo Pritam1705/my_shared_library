8431697f18d20ed939e5acc83b78095ddfbb6262
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-12 11:34:44 +0530

    Update gitCheckOut.groovy

--- src/org/p11combinedforce/common/gitCheckOut.groovy
+++ src/org/p11combinedforce/common/gitCheckOut.groovy
@@ -2,14 +2,8 @@ package org.p11combinedforce.common
 
 def call(String url, String branch, String creds) {
     stage("Clone Private Repo") {
-        script {
-            checkout([$class: 'GitSCM', 
-                branches: [[name: branch]],
-                userRemoteConfigs: [[
-                    url: url,
-                    credentialsId: creds
-                ]]
-            ])
+        script{
+                git branch: branch, url: url, credentialsId: creds
         }
     }
 }
