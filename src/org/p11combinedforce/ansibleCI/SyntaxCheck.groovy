601710a6ad31e56405ef664add3c945995ff563d
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-12 11:34:20 +0530

    Update SyntaxCheck.groovy

--- src/org/p11combinedforce/ansibleCI/SyntaxCheck.groovy
+++ src/org/p11combinedforce/ansibleCI/SyntaxCheck.groovy
@@ -1,9 +1,9 @@
 package org.p11combinedforce.ansibleCI
 
-def call(String playbookPath) {
+def call(String playbookName) {
     stage("Syntax Check") {
         script {
-            sh "ansible-playbook --syntax-check ${playbookPath}"
+            sh "ansible-playbook --syntax-check ${playbookName}"
         }
     }
 }
