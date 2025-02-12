2519aa0981d20cbf18cb92263b824ffc368a2519
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-12 11:07:44 +0530

    Create ansibleCI.groovy

---
+++ src/org/p11combinedforce/template/ansibleCI/ansibleCI.groovy
@@ -0,0 +1,17 @@
+package org.p11combinedforce.ansibleCI
+
+import org.p11combinedforce.ansibleCI.*
+import org.p11combinedforce.common.*
+
+def call(String url, String branch, String creds, String playbookName) {
+
+    // Create instances of required classes
+     def gitCheckOut = new gitCheckOut()
+     def wsClean=new wsClean()
+     def SyntaxCheck=new SyntaxCheck()
+
+    // Call methods with correct arguments
+    wsClean.call()
+    gitCheckOut.call(url, branch, creds)  
+    SyntaxCheck.call(playbookName)
+}
